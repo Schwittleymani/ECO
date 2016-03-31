f0d2e20da6bafa45eaa8997bd572e85bd48ee28d
Marcel Schwittlick <marzzzel@gmail.com>
2016-03-31 19:55:12 +0200

    adds very first start

---
+++ src/main/java/oracle/LacunaLabOracle.java
@@ -0,0 +1,20 @@
+package oracle;
+
+import processing.core.PApplet;
+
+/**
+ * Created by mrzl on 31.03.2016.
+ */
+public class LacunaLabOracle extends PApplet {
+
+    public void settings() {
+        size(1920, 1080);
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+
+    }
+}
---
+++ src/main/java/oracle/Main.java
@@ -0,0 +1,12 @@
+package oracle;
+
+import processing.core.PApplet;
+
+/**
+ * Created by mrzl on 31.03.2016.
+ */
+public class Main {
+    public static void main ( String[] args ) {
+        PApplet.main( "oracle.LacunaLabOracle" );
+    }
+}
