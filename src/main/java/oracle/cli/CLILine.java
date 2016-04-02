b7e793bf2d83efd0a392488172f8b2933abb8083
Ramin <ramin.soleymani@ixds.com>
2016-04-02 21:46:08 +0200

    use inputPreChars

--- src/main/java/oracle/cli/CLILine.java
+++ src/main/java/oracle/cli/CLILine.java
@@ -12,7 +12,7 @@ public class CLILine extends Line {
     }
 
     public void backspace () {
-        if ( size() > 4 ) {
+        if ( size() > CLI.inputPreChars.length() ) {
             remove( size( ) - 1 );
         }
     }
