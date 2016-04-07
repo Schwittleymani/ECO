21e6761720ca609c1d48facf399f6900a89a04a7
Ramin <ramin.soleymani@ixds.com>
2016-04-07 14:12:47 +0200

    spellchecker hello

---
+++ src/main/java/oracle/util/Spellchecker.java
@@ -0,0 +1,19 @@
+package oracle.util;
+
+import com.inet.jortho.SpellChecker;
+
+import java.net.URL;
+
+/**
+ * Created by raminsoleymani on 03/04/16.
+ */
+public class Spellchecker {
+
+
+    public Spellchecker() {
+       // SpellChecker.setUserDictionaryProvider( new MyUserDictionaryProvider() );
+
+       // SpellChecker.registerDictionaries( new URL("file", null, ""), "en,de", "de" );
+    }
+
+}
