a7319c93950665ce03f08783350cf4d575735eae
Marcel Schwittlick <marzzzel@gmail.com>
2016-04-08 00:51:50 +0200

    makes font color changable. closes #22

--- src/main/java/oracle/cli/CLI.java
+++ src/main/java/oracle/cli/CLI.java
@@ -1,13 +1,10 @@
 package oracle.cli;
 
-import oracle.ostern.EasterEgg;
-import oracle.ostern.EmojiEasterEgg;
 import oracle.ostern.Jesus;
 import processing.core.PApplet;
 import processing.core.PFont;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by mrzl on 31.03.2016.
@@ -27,19 +24,16 @@ public class CLI {
     float cursorBlockWidth;
     public static String inputPreChars = "[ ] ";
 
-
-
     public CLI ( PApplet p ) {
         this.parent = p;
         this.font = p.createFont( "Glass_TTY_VT220.ttf", textSize );
         p.textFont( this.font );
 
         reset( );
-        jesus = new Jesus(p);
+        jesus = new Jesus( p );
 
         parent.textSize( textSize );
         setupWidth( );
-
     }
 
     public void setupWidth () {
@@ -54,15 +48,16 @@ public class CLI {
     }
 
     public void draw () {
-        jesus.drawBeforeEaster();
-
         parent.fill( 0, 255, 0 );
+
+        jesus.drawBeforeEaster( );
+
         pushLinesUp( );
         lines.forEach( Line::draw );
 
         drawBlinkingLine( );
 
-        jesus.drawAfterEaster();
+        jesus.drawAfterEaster( );
     }
 
     private void pushLinesUp () {
@@ -167,7 +162,6 @@ public class CLI {
     }
 
     public void emptyInput () {
-        jesus.start(Jesus.EASTEREGG_TYPE.EMOJI, 10);
+        jesus.start( Jesus.EASTEREGG_TYPE.EMOJI, 10 );
     }
-
 }
