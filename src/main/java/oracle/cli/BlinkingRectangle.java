ffb36fa257ddb461265e998c75ea3a963c95aac4
Marcel Schwittlick <marzzzel@gmail.com>
2016-04-09 18:58:31 +0200

    moves blinking line into class

---
+++ src/main/java/oracle/cli/BlinkingRectangle.java
@@ -0,0 +1,33 @@
+package oracle.cli;
+
+import processing.core.PApplet;
+
+/**
+ * Created by mrzl on 09.04.2016.
+ */
+public class BlinkingRectangle{
+    private PApplet parent;
+    private CLI cli;
+
+    public BlinkingRectangle( PApplet parent, CLI cli ) {
+        this.parent = parent;
+        this.cli = cli;
+    }
+
+    public void draw() {
+        parent.pushStyle();
+        parent.strokeWeight( 2 );
+
+        int textColor = ( parent.millis() % ( 255 * 4 ) ) / 4;
+        parent.stroke( 0, textColor, 0 );
+        float textWidth = parent.textWidth( cli.getLastLine().getText() );
+        if( parent.random( 1 ) < 0.04f ){
+            parent.noFill();
+        }
+        parent.rect( 50 + textWidth, cli.getLastLine().y - ( cli.textSize ),
+                cli.cursorBlockWidth, cli.textSize + 10 * parent.noise( parent.frameCount * 0.01f ) );
+
+
+        parent.popStyle();
+    }
+}
