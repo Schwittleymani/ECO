5f8fcc5b75966547fb9974099a000f22a57aed50
Ramin <ramin.soleymani@ixds.com>
2016-09-18 23:01:59 +0200

    added example for enchant. does some okay spell checking but doesn't find the best words on the spot #69

--- src/python/webserver/grammar_enchant.py
+++ src/python/webserver/grammar_enchant.py
@@ -2,10 +2,12 @@ import enchant
 import enchant.checker
 from enchant.checker.CmdLineChecker import CmdLineChecker
 
-chkr = enchant.checker.SpellChecker("en_US","this is sme example txt")
+chkr = enchant.checker.SpellChecker("en_US")
+chkr.set_text("this is sme example txt")
 for err in chkr:
     print "ERROR:", err.word
-#cmdln = CmdLineChecker()
-#cmdln.set_checker(chkr)
-#cmdln.run()
+    suggestions = chkr.suggest(err.word)
+    if chkr.suggest(len(err.word) > 0):
+        err.replace(suggestions[0])
+
 print chkr.get_text()
