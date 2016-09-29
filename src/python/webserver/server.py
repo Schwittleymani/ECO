711e6301f6aac6ed6361b7d779584467591e751d
Ramin <ramin.soleymani@ixds.com>
2016-09-29 18:18:48 +0200

    kicked websockets out again, so its killable

--- src/python/webserver/server.py
+++ src/python/webserver/server.py
@@ -42,7 +42,7 @@ def launch():
     def start_websocket_server():
 
         socketio.run(app, port=app.config['PORT_WEBSOCKET'])
-    Thread(target=start_websocket_server).start()
+    #Thread(target=start_websocket_server).start()
     app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'])
 
     #logger.debug("Application terminates")
