07045e88c44434798fe39c5697315af3e2b62323
Marcel Schwittlick <lzrm@mrzl.net>
2016-11-06 13:18:52 +0800

    merging pdf to text conversion and text parsing into one utility

--- src/python/utils/batch_postprocess_text.py
+++ src/python/utils/batch_postprocess_text.py
@@ -1,22 +1,50 @@
 import os
 import sys
+import argparse
 
-source_folder = '/mnt/drive1/data/eco/pdf_to_text/'
-dest_folder = '/mnt/drive1/data/eco/pdf_to_text_postprocessed/'
+import textract
+import textparser
 
-if len(sys.argv) > 1:
-    source_folder = sys.argv[1] + "/"
 
-if len(sys.argv) > 2:
-    dest_folder = sys.argv[2] + "/"
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description='convert pdfs to text and parse the texts properly')
 
-if not os.path.exists(dest_folder):
-    os.makedirs(dest_folder)
+    parser.add_argument('--input_path', action='store', help='path to folder with pdf files')
+    parser.add_argument('--output_path', action='store', help='path to save text files to')
+    params = vars(parser.parse_args(args))
 
-for file in os.listdir(source_folder):
-    print file
-    print source_folder + file
-    cmd = "pdftotext " + source_folder + file + " " + dest_folder + file[:-3] + "txt"
-    cmd = 'python /home/marcel/projects/eco/src/python/utils/text_postprocessor.py --input ' + source_folder + file + ' --output ' + dest_folder + file
-    print cmd
-    os.system(cmd)
+    return params
+
+if __name__ == '__main__':
+    params = process_arguments(sys.argv[1:])
+
+    input_path = params['input_path']
+    output_path = params['output_path']
+
+    if input_path is None or output_path is None:
+        print('Missing parameters.')
+        sys.exit(1)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for file in os.listdir(input_path):
+        if not file.endswith('.pdf'):
+            continue
+
+        print('Parsing ' + input_path + file)
+
+        try:
+            text = textract.process(input_path + file)
+
+            parser = textparser.TextParser()
+            parser.parse(text.decode('utf-8'))
+
+            output_filename = output_path + file[:-3] + '.txt'
+            print('Saving to ' + output_filename)
+            output_file = open(output_filename, 'w')
+            for line in parser.proper_sentences:
+                output_file.write(line.string.encode('utf8'))
+                output_file.write('\n')
+        except TypeError as e:
+            print(e)
