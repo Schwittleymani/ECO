c99f8c85f5c983835fbcd36583266282aa54e040
Marcel Schwittlick <lzrm@mrzl.net>
2016-11-28 19:20:19 +0800

    adjusts the text parser, writing statistics

--- src/python/utils/batch_postprocess_text.py
+++ src/python/utils/batch_postprocess_text.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import argparse
+import glob
 
 import textract
 import textparser
+
+import matplotlib.pyplot as plt
 
 
 def process_arguments(args):
@@ -14,6 +17,51 @@ def process_arguments(args):
     params = vars(parser.parse_args(args))
 
     return params
+
+def render_statistic(parser):
+    print('Statistic')
+    print('All sentences: ' + str(parser.statistic.all_sentences))
+    print('Proper sentences: ' + str(len(parser.proper_sentences)))
+    print('Too few words: ' + str(parser.statistic.too_few_words))
+    print('First word number: ' + str(parser.statistic.first_word_is_number))
+    print('Contains brackets: ' + str(parser.statistic.sentence_contains_brackets))
+    print('Contains number: ' + str(parser.statistic.sentence_contains_number))
+    print('Too many comma: ' + str(parser.statistic.sentence_too_many_comma))
+
+    labels = 'Proper Sentences', 'Too Few Words', 'First Word Number', 'Contains Brackets', 'Contains Number', 'Too Many Comma'
+    sizes = [len(parser.proper_sentences), parser.statistic.too_few_words, parser.statistic.first_word_is_number, parser.statistic.sentence_contains_brackets, parser.statistic.sentence_contains_number, parser.statistic.sentence_too_many_comma]
+    colors = ['yellowgreen', 'mediumpurple', 'lightskyblue', 'lightcoral', 'darkred', 'yellow']
+    explode = (0, 0, 0, 0, 0, 0)    # proportion with which to offset each wedge
+
+    def make_autopct(values):
+        def my_autopct(pct):
+            total = sum(values)
+            val = int(round(pct*total/100.0))
+            return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+        return my_autopct
+    plt.pie(sizes,              # data
+    explode=explode,    # offset parameters
+    labels=labels,      # slice labels
+    colors=colors,      # array of colours
+    autopct=make_autopct(sizes),  # print the values inside the wedges
+    shadow=True,        # enable shadow
+    startangle=70       # starting angle
+    )
+
+    plt.axis('equal')
+    #plt.show()
+
+    file = open('statistics.txt', 'w')
+    file.write('Files parsed: ' + str(parser.statistic.files_parsed)+'\n')
+    file.write('All sentences: ' + str(parser.statistic.all_sentences)+'\n')
+    file.write('Proper sentences: ' + str(len(parser.proper_sentences))+'\n')
+    file.write('Too few words: ' + str(parser.statistic.too_few_words)+'\n')
+    file.write('First word number: ' + str(parser.statistic.first_word_is_number)+'\n')
+    file.write('Contains brackets: ' + str(parser.statistic.sentence_contains_brackets)+'\n')
+    file.write('Contains number: ' + str(parser.statistic.sentence_contains_number)+'\n')
+    file.write('Too many comma: ' + str(parser.statistic.sentence_too_many_comma)+'\n')
+    file.close()
+
 
 if __name__ == '__main__':
     params = process_arguments(sys.argv[1:])
@@ -28,24 +76,28 @@ if __name__ == '__main__':
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    for file in os.listdir(input_path):
-        if not file.endswith('.pdf'):
-            continue
+    parser = textparser.TextParser()
+    for file_path in glob.glob(input_path + '/*.pdf'):
+        print('Parsing ' + file_path)
 
-        print('Parsing ' + input_path + file)
+        path, filename = os.path.split(file_path)
 
         try:
-            text = textract.process(input_path + file)
-
-            parser = textparser.TextParser()
+            text = textract.process(file_path)
             parser.parse(text.decode('utf-8'))
 
-            output_filename = output_path + file[:-3] + 'txt'
+            output_filename = output_path + filename[:-3]
+            output_filename += 'txt'
+            output_filename = output_filename.replace(' ', '_')
+            output_filename = output_filename.replace('.', '_')
             print('Saving to ' + output_filename)
             output_file = open(output_filename, 'w')
             for line in parser.proper_sentences:
                 output_file.write(line.string.encode('utf8'))
                 output_file.write('\n')
+
+            render_statistic(parser)
+
         except TypeError as e:
             print(e)
         except textract.exceptions.ShellError as e:
--- src/python/utils/textparser.py
+++ src/python/utils/textparser.py
@@ -1,9 +1,29 @@
 from pattern.en import parse, Text
 
+class ParseStatistic(object):
+    all_sentences = 0
+    proper_sentences = 0
+    files_parsed = 0
+
+    too_few_words = 0
+    first_word_is_number = 0
+    sentence_contains_brackets = 0
+    sentence_contains_number = 0
+    sentence_too_many_comma = 0
+
 class TextParser(object):
+    """
+    This parses a massive string, that contains an entire book. Splits
+    it into sentences and either discards the sentence if the pdf->txt
+    conversion was faulty already or returns it.
+
+    This is using pattern.en as a analysis library http://www.clips.ua.ac.be/pages/pattern-en#parser
+    The tags of words are being analyzed: http://www.clips.ua.ac.be/pages/mbsp-tags
+    """
     def __init__(self):
         self.proper_sentences = []
         self.MIN_WORD_COUNT = 10
+        self.statistic = ParseStatistic()
 
     @staticmethod
     def get_count_tag(sentence, tag):
@@ -22,6 +42,7 @@ class TextParser(object):
         return False
 
     def parse(self, text):
+        self.proper_sentences = []
         text = Text(parse(text,
                           tokenize=True,
                           tags=True,
@@ -31,7 +52,8 @@ class TextParser(object):
                           encoding='utf-8',
                           tagset=None))
 
-        index = 0
+        self.statistic.all_sentences += len(text)
+        self.statistic.files_parsed += 1
         for sentence in text:
             if len(sentence.words) > self.MIN_WORD_COUNT:
                 first_word = sentence.words[0]
@@ -43,6 +65,21 @@ class TextParser(object):
                         if self.get_count_tag(sentence, "CD") < 1:
                             if self.get_count_tag(sentence, ",") < 3:
                                 self.proper_sentences.append(sentence)
-            index += 1
-
+                            else:
+                                # the sentence has more than 2 occurrences of a comma(,)
+                                self.statistic.sentence_too_many_comma += 1
+                        else:
+                            # the sentence contains a number
+                            self.statistic.sentence_contains_number += 1
+                    else:
+                        # the sentence contains either ( ) or \
+                        self.statistic.sentence_contains_brackets += 1
+                else:
+                    # first word of the sentence is a number
+                    self.statistic.first_word_is_number += 1
+            else:
+                # too few words in the sentence
+                # removes sentences like these: https://gist.github.com/mrzl/32b9763bd943c18cb77cd1167a87640a
+                self.statistic.too_few_words += 1
+        self.statistic.proper_sentences += len(self.proper_sentences)
         print('Parsed ' + str(len(self.proper_sentences)) + ' proper sentences.')
