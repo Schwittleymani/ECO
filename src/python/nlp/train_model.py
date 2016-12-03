9985e7168aec1a09446acd057be1422782547dbb
Marcel Schwittlick <lzrm@mrzl.net>
2016-12-03 13:39:06 +0800

    works on the word2vec model train module. related to #162

---
+++ src/python/nlp/train_model.py
@@ -0,0 +1,78 @@
+import gensim
+import math
+import sys
+import os
+import pathlib
+import glob
+import argparse
+import multiprocessing
+import logging
+
+
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description='configure Word2Vec model building')
+    parser.add_argument('--input_path', action='store', help='the path to a folder containing sentence txt file')
+    parser.add_argument('--verbose', action='store_true', help='toggle to enable verbose output of training process')
+    params = vars(parser.parse_args(args))
+    return params
+
+class Sentence(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        text_files = glob.glob(self.dirname + '/*.txt')
+        for file in text_files:
+            for line in open(file):
+                # line = line.lower()
+                yield line.split()
+
+def get_last_dir_from_path(path):
+    list = path.split('/')
+    if path.endswith('/'):
+        out = list[-2]
+    else:
+        out = list[-1]
+    return out
+
+def enable_verbose_training():
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+def train_model(folder_path):
+    model = gensim.models.Word2Vec(Sentence(folder_path), size=400, min_count=5, negative=5, window=10, workers=multiprocessing.cpu_count())
+    model.init_sims(replace=True)
+    print('Finished training: ' + str(model) + ' Filesize:', str(round(model.estimate_memory()['total'] / (math.pow(1024, 2)))) + 'mb')
+    return model
+
+def export_model(input_path):
+    # the input path is used for constructing the filename of the model
+    model_filename = get_last_dir_from_path(input_path) + '.model'
+    # file exists already in current directory?
+    if pathlib.Path(model_filename).is_file():
+        # y/n choice to overwrite
+        user_input = input('Overwrite model file ' + model_filename + '? [y/(n)]')
+        if 'y' in user_input:
+            model.save(model_filename)
+            print('Saved model: ' + model_filename)
+        else:
+            print('NOT saved model. File already exists.')
+    else:
+        model.save(model_filename)
+        print('Saved model: ' + model_filename)
+
+
+if __name__ == '__main__':
+    params = process_arguments(sys.argv[1:])
+    input_path = params['input_path']
+    verbose = params['verbose']
+
+    if verbose:
+        enable_verbose_training()
+
+    model = train_model(input_path)
+    export_model(input_path)
