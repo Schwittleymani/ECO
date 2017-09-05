96b3ab6c08c9685e0a957671c3c0267081f22144
Marcel Schwittlick <marzzzel@gmail.com>
2017-09-05 21:09:22 +0200

    fixes darknet shared lib path

--- src/v4/misc/darknet/darknet_classify.py
+++ src/v4/misc/darknet/darknet_classify.py
@@ -37,7 +37,7 @@ class METADATA(Structure):
                 ("names", POINTER(c_char_p))]
 
 
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -156,7 +156,7 @@ if __name__ == "__main__":
                         free_image(im)
                 else:
                     print("rror")
-                
+
                 img.close()
         except:
             pass
