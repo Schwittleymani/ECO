4075dc022359184f89acc4836aaf2197e2bfbdab
Marcel Schwittlick <marzzzel@gmail.com>
2017-09-14 12:21:06 +0200

    adds non broadcast emit on connect

--- src/v4/server/webserver.py
+++ src/v4/server/webserver.py
@@ -19,19 +19,17 @@ def msg():
     data = request.get_json()
     if 'text' not in data or 'user' not in data:
         return json.dumps({'error': 'text or user missing'}), 400, {'ContentType': 'application/json'}
-    send_msg(data)
+    send_msg(data, bcast=True)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @socketio.on('connect')
 def client_connect():
-    send_msg({"text":'hello', "user":'connected'})
+    send_msg({'text': 'hello', 'user': 'connected'})
 
 
-def send_msg(data):
-    # print('send_msg')
-    socketio.emit('msg', data, broadcast=True)
-
+def send_msg(data, bcast=False):
+    socketio.emit('msg', data, broadcast=bcast)
 
 if __name__ == "__main__":
     socketio.run(app, host=settings.HOST, port=settings.PORT, debug=settings.DEBUG)
