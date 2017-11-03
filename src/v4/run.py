b6af7432121e3718c15ee1d63df63805c2bca868
Marcel Schwittlick <marzzzel@gmail.com>
2017-11-03 18:09:30 +0100

    text length relative to delay time

--- src/v4/run.py
+++ src/v4/run.py
@@ -2,7 +2,20 @@ import requests
 import time
 import random
 import argparse
+import json
 import sys
+
+
+def map(value, istart, istop, ostart, ostop):
+    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+
+
+def text_to_wait(text):
+    text_length = len(text)
+    wait_seconds = map(text_length, 0, 100, 1, 10)
+    wait_seconds = min(wait_seconds, 15)
+    print('text_length: ' + str(text_length) + ' waiting: ' + str(wait_seconds))
+    return wait_seconds
 
 
 def process_arguments(args):
@@ -20,16 +33,36 @@ def run_live():
     postmanager = PostManager()
     while True:
         post = postmanager.add_random()
-        json_to_send = postmanager.last().json()
+
+        json_to_send = post.json()
         print('sent: ' + json_to_send)
         result = requests.post('http://localhost:8090/msg', json_to_send, headers={'Content-Type': 'application/json'})
         if not result.ok:
             print(result.__dict__)
 
         if isinstance(post, InteractivePost):
-            time.sleep(7)
+            time.sleep(10)
         else:
-            time.sleep(random.uniform(1, 4))
+            wait_seconds = text_to_wait(post.text())
+            time.sleep(wait_seconds)
+
+
+def run_replay(replay_file_path):
+    replay_lines = []
+    replay_file = open(replay_file_path, 'r')
+    for line in replay_file.readlines():
+        replay_lines.append(line)
+    replay_file.close()
+
+    while True:
+        for line in replay_lines:
+            print('sending: ' + line)
+            result = requests.post('http://localhost:8090/msg', line, headers={'Content-Type': 'application/json'})
+            if not result.ok:
+                print(result.__dict__)
+
+            wait_seconds = text_to_wait(json.loads(line)['text'])
+            time.sleep(wait_seconds)
 
 
 if __name__ == "__main__":
@@ -38,19 +71,6 @@ if __name__ == "__main__":
     replay_path = params['replay']
 
     if replay_path:
-        replay_lines = []
-        replay_file = open(replay_path, 'r')
-        for line in replay_file.readlines():
-            replay_lines.append(line)
-        replay_file.close()
-
-        while True:
-            for line in replay_lines:
-                print('sending: ' + line)
-                result = requests.post('http://localhost:8090/msg', line, headers={'Content-Type': 'application/json'})
-                if not result.ok:
-                    print(result.__dict__)
-
-                time.sleep(random.uniform(1, 4))
+        run_replay(replay_path)
     else:
         run_live()
